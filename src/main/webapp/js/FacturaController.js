f3d3643f482a40d11dfdba63e22053b96b675f03
Karol Tatiana Alfonso G. <karol.alfonso@uptc.edu.co>
2019-02-18 09:30:57 -0500

    YA CASI ESTA!!

--- src/main/webapp/js/FacturaController.js
+++ src/main/webapp/js/FacturaController.js
@@ -1,8 +1,8 @@
 //var listaProductos = [{id: 1, nombre: 'papas', valor: 200}, {id: 2, nombre: 'pan', valor: 200}];
 module.controller('FacturaController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
         $scope.listaProductos = null;
-        $scope.listaDetalles = null;
-        $scope.id = 3;
+        $scope.listaDetallesAux = [];
+        $scope.id = 1;
         $scope.datosDetalleFactura = {};
 
         $scope.getProducto = function () {
@@ -19,7 +19,7 @@ module.controller('FacturaController', ['$scope', '$filter', '$http', function (
 
         $scope.listaFactura = null;
 //factura
-$scope.getFactura = function () {
+        $scope.getFactura = function () {
             $http.get("./webresources/ServicioFactura", {})
                     .then(function (response) {
                         console.log(response.data);
@@ -44,32 +44,49 @@ $scope.getFactura = function () {
             $http.post("./webresources/ServicioFactura", $scope.datosFactura)
                     .then(function (response) {
                         console.log(response.data);
-                       $scope.getFactura();
+                        $scope.getFactura();
                     }, function () {
                         alert("error");
                     });
         }
         $scope.agregar = function (data) {
+            $scope.datosDF = {}
             //$scope.listaDetalles.factura.id = 
-            
+            console.log('agregado a carrito');
+            console.log($scope.listaProductos.length);
+          
             for (var i = 0; i < $scope.listaProductos.length; i++) {
-
                 if ($scope.listaProductos[i] == data) {
+                       
+                    $scope.datosDF.idDetalle = $scope.id++;
+                    $scope.datosDF.producto = $scope.listaProductos[i];
+                    $scope.datosDF.totalProducto = data.valor * $scope.datosDetalleFactura.cantidad[i];
+                    $scope.datosDF.cantidad = $scope.datosDetalleFactura.cantidad;
+//                    console.log('aqui voy' +  $scope.datosDetalleFactura.idDetalle + "p" + $scope.datosDetalleFactura.producto.nombre);
+//                    $scope.listaDetalles.push($scope.datosDetalleFactura);
+                    
+                    $scope.datosFactura.listaDetalleF.push($scope.datosDF.valueOf());
+                        console.log($scope.datosFactura);
+                    
 
-                    $scope.datosDetalleFactura.producto = $scope.listaProductos[i];
-                    $scope.datosDetalleFactura.totalProducto = data.valor * $scope.datosDetalleFactura.cantidad;
-                    $scope.listaDetalles.push($scope.datosDetalleFactura);
-                    $scope.datosFactura.listaDetalleF.push($scope.datosDetalleFactura);
-
-                    console.log($scope.datosFactura);
+                   
                 }
             }
+            var suma = 0;
+            for (var i = 0; i <  $scope.datosFactura.listaDetalleF.length; i++) {
+                
+                suma += $scope.datosFactura.listaDetalleF[i].totalProducto;
+                console.log(suma + "= suma")
+                  $scope.datosFactura.valorFactura = suma;
+            }
+               
+              
 //        $scope.listaProductos={};
         }
         $scope.comprar = function () {
-            
+
             $scope.datosFactura.valorFactura = 60000;
-            console.log("holi" +  $scope.datosFactura.valorFactura );
+            console.log("holi" + $scope.datosFactura.valorFactura);
             $scope.guardarFactura();
         }
 
@@ -77,7 +94,9 @@ $scope.getFactura = function () {
             $http.get("./webresources/ServicioDetalleFactura", {})
                     .then(function (response) {
                         console.log(response.data);
-                        $scope.listaDetalles = response.data;
+                        for (var i = 0; $scope.datosFactura.listaDetalleF.length; i++) {
+                            $scope.datosFactura.listaDetalleF = response.data;
+                        }
                     }, function () {
                         alert("error");
                     });
